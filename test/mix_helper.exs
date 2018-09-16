89a3982a3936a669e3f23bb0c962b661c8c12126
Milton Mazzarri <milmazz@gmail.com>
2018-09-16 15:06:17 -0500

    Add helper functions

---
+++ test/mix_helper.exs
@@ -0,0 +1,50 @@
+defmodule ScenicNew.MixHelper do
+  # NOTE: Most of these helper functions were borrowed from the Phoenix
+  # installer
+  import ExUnit.Assertions
+
+  def tmp_path do
+    Path.expand("../../tmp", __DIR__)
+  end
+
+  defp random_string(len) do
+    len |> :crypto.strong_rand_bytes() |> Base.encode64() |> binary_part(0, len)
+  end
+
+  def in_tmp(which, function) do
+    path = Path.join([tmp_path(), random_string(10), to_string(which)])
+
+    try do
+      File.rm_rf!(path)
+      File.mkdir_p!(path)
+      File.cd!(path, function)
+    after
+      File.rm_rf!(path)
+    end
+  end
+
+  def assert_file(file) do
+    assert File.regular?(file), "Expected #{file} to exist, but does not"
+  end
+
+  def refute_file(file) do
+    refute File.regular?(file), "Expected #{file} to not exist, but it does"
+  end
+
+  def assert_file(file, match) do
+    cond do
+      is_list(match) ->
+        assert_file(file, &Enum.each(match, fn m -> assert &1 =~ m end))
+
+      is_binary(match) or Regex.regex?(match) ->
+        assert_file(file, &assert(&1 =~ match))
+
+      is_function(match, 1) ->
+        assert_file(file)
+        match.(File.read!(file))
+
+      true ->
+        raise inspect({file, match})
+    end
+  end
+end
