d97b1b9c76c4058fd1bb06b072aec34af2d2b672
Jason Axelson <jason.axelson@gmail.com>
2019-08-03 09:05:28 -1000

    Pull in more changes from a plain nerves generated project
    I compared the output of this generator with the output of `mix nerves.new
    nnerves` using a commnand like:
    
        diff -r --color -x nnerves.ex -x .formatter.exs -x .gitignore -x mix.exs \
          -x README.md nnerves/ nerves_gen/nnerves/
    
    Now the differences are very minor, largest is probably the erlinit.config
    Also fixed not importing `target.exs` correctly, although it wouldn't have
    caused any major issues besides networking not working.

--- templates/new_nerves/rootfs_overlay/etc/iex.exs
+++ templates/new_nerves/rootfs_overlay/etc/iex.exs
@@ -1,17 +1,17 @@
-# Pull in Nerves-specific helpers to the IEx session
-use Nerves.Runtime.Helpers
+# Add Toolshed helpers to the IEx session
+use Toolshed
 
 if RingLogger in Application.get_env(:logger, :backends, []) do
-  IO.puts """
+  IO.puts("""
   RingLogger is collecting log messages from Elixir and Linux. To see the
   messages, either attach the current IEx session to the logger:
 
     RingLogger.attach
 
-  or tail the log:
+  or print the next messages in the log:
 
-    RingLogger.tail
-  """
+    RingLogger.next
+  """)
 end
 
 # Be careful when adding to this file. Nearly any error can crash the VM and
