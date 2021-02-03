dcb58e0769ecf28b52a0eab6819df0208b6fb887
crertel <chris@kedagital.com>
2021-02-02 20:01:32 -0600

    Add mix scenic.gen.scene first cut.

---
+++ lib/mix/tasks/scenic.gen.scene.ex
@@ -0,0 +1,71 @@
+defmodule Mix.Tasks.Scenic.Gen.Scene do
+  @shortdoc "Creates scaffolding for a new Scenic scene."
+
+  @moduledoc """
+  Generates Scene boilerplate for a new scene.
+
+  ```bash
+  mix scenic.gen.scene scene_name
+  ```
+  """
+  use Mix.Task
+
+  import Mix.Generator
+  alias ScenicNew.Common
+
+  @switches [
+    app: :string,
+    module: :string
+  ]
+
+  # --------------------------------------------------------
+  @doc false
+  def run(argv) do
+    {opts, argv} = OptionParser.parse!(argv, strict: @switches)
+
+    case argv do
+      [] ->
+        Mix.Tasks.Help.run(["scenic.gen.scene"])
+
+      [scene_name | _] ->
+
+        Common.elixir_version_check!()
+        app = opts[:app] || Path.basename(Path.expand(scene_name))
+        Common.check_application_name!(app, !opts[:app])
+        mod = opts[:module] || Macro.camelize(app)
+        Common.check_mod_name_validity!(mod)
+        Common.check_mod_name_availability!(mod)
+
+        generate(app, mod)
+    end
+  end
+
+  # --------------------------------------------------------
+  defp generate(component_filename, component_module) do
+
+    assigns = [
+      app_module_name: component_module, #TOD: Figure out the right module name.
+      scene_module_name: component_module
+    ]
+
+    component_path = "lib/scenes/#{component_filename}.ex"
+    #IO.puts("Generating at #{component_filename} #{component_module} #{component_path}")
+
+    create_directory("lib/scenes")
+    create_file(component_path, scene_new_template(assigns))
+
+    """
+    Created component #{component_filename}.
+    """
+    |> Mix.shell().info()
+  end
+
+  # ============================================================================
+  # template files
+  [
+    scene_new: "templates/gen_scene/lib/scenes/scene.ex.eex"
+  ]
+  |> Enum.each(fn {name, content} ->
+    embed_template(name, from_file: content)
+  end)
+end
---
+++ test/scenic_gen_scene_test.exs
@@ -0,0 +1,62 @@
+Code.require_file("mix_helper.exs", __DIR__)
+
+defmodule Mix.Tasks.Scenic.Gen.SceneTest do
+  use ExUnit.Case, async: false
+  @moduletag :gen_scene
+
+
+  import ScenicNew.MixHelper
+  import ExUnit.CaptureIO
+
+  @scene_name "test_scene"
+  @scene_module_name "TestScene"
+  @app_name "scenic_demo"
+  @module_name "ScenicDemo"
+
+  test "gen.scene" do
+     in_tmp("new with defaults", fn ->
+       assert capture_io(fn ->
+      Mix.Tasks.Scenic.Gen.Scene.run([@scene_name])
+
+      assert_file("lib/scenes/#{@scene_name}.ex", fn file ->
+        assert file =~ "defmodule #{@scene_module_name} do"
+      end)
+    end) =~ "Created component #{@scene_module_name}."
+  end)
+  end
+
+  # test "new with invalid args" do
+  #   assert_raise Mix.Error, ~r"Application name must start with a lowercase ASCII letter,", fn ->
+  #     Mix.Tasks.Scenic.Gen.Scene.run(["007invalid"])
+  #   end
+
+  #   assert_raise Mix.Error, ~r"Application name must start with a lowercase ASCII letter, ", fn ->
+  #     Mix.Tasks.Scenic.Gen.Scene.run(["valid", "--app", "007invalid"])
+  #   end
+
+  #   assert_raise Mix.Error, ~r"Module name must be a valid Elixir alias", fn ->
+  #     Mix.Tasks.Scenic.Gen.Scene.run(["valid", "--module", "not.valid"])
+  #   end
+
+  #   assert_raise Mix.Error, ~r"Module name \w+ is already taken", fn ->
+  #     Mix.Tasks.Scenic.Gen.Scene.run(["string"])
+  #   end
+
+  #   assert_raise Mix.Error, ~r"Module name \w+ is already taken", fn ->
+  #     Mix.Tasks.Scenic.Gen.Scene.run(["string", "chars"])
+  #   end
+
+  #   assert_raise Mix.Error, ~r"Module name \w+ is already taken", fn ->
+  #     Mix.Tasks.Scenic.Gen.Scene.run(["valid", "--app", "mix"])
+  #   end
+
+  #   assert_raise Mix.Error, ~r"Module name \w+ is already taken", fn ->
+  #     Mix.Tasks.Scenic.Gen.Scene.run(["valid", "--module", "String"])
+  #   end
+  # end
+
+  # test "new without args" do
+  #   assert capture_io(fn -> Mix.Tasks.Scenic.Gen.Scene.run([]) end) =~
+  #   "Generates Scene boilerplate for a new scene."
+  # end
+end
